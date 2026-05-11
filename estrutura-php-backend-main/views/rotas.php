0000d3f9fbef6d6979cd20a470fc77b2b6eb020c
Mateus Lira <167817600+MateusLLopes@users.noreply.github.com>
2026-05-11 14:33:11 -0300

    Delete estrutura-php-backend-main directory

--- estrutura-php-backend-main/views/rotas.php
+++
@@ -1,7 +0,0 @@
- <?php
-    $paginasPermitidas = [
-        'index' => 'pagina-inicial.php',
-        'home' => 'pagina-inicial.php',
-        'categorias' => 'categoria/consultar.php',
-        'add/categoria' => 'categoria/salvar.php',
-    ];
